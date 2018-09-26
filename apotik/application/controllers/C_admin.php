0b271d8e412992253de8e21406c5e2bb9679391c
Febry Triyadi <triyadifebry@gmail.com>
2018-09-26 22:05:03 +0700

    Update C_admin.php

--- apotik/application/controllers/C_admin.php
+++ apotik/application/controllers/C_admin.php
@@ -6,29 +6,31 @@
         public function __construct(){
             parent::__construct();
 			$this->load->helper(array('url','form'));
-			#$this->load->model('m_user');
+			#$this->load->model('m_staff');
 			$this->load->model('m_obat');
             $this->load->model('m_laporan');
             $this->load->model('m_pesanan');
+            $this->load->model('m_komplain');
 			$this->load->library('session');
         }
         public function index(){
+            $data['data']=$this->m_obat->getobat();
+            $this->load->view('v_index',$data);
+        }
+        public function data_obat(){
             $data['data']=$this->m_obat->getobat();
             $this->load->view('data_obat',$data);
         }
         public function searchmobil(){
             $this->load->view('searchmobil');
         }
-        public function searchmotor(){
-            $this->load->view('searchmotor');
-        }
         public function data_pesanan(){
             $data['data']=$this->m_pesanan->getpesanan();
             $this->load->view('data_pesanan',$data);
         }
-        public function pilihmotor(){
-            $data['data']=$this->m_motor->getmotor();
-            $this->load->view('pilihmotor',$data);
+        public function data_komplain(){
+            $data['data']=$this->m_komplain->getkomplain();
+            $this->load->view('data_komplain',$data);
         }
         public function tambahobat(){
             $data['data']=$this->m_obat->getobat();
@@ -38,84 +40,44 @@
             $data['data']=$this->m_laporan->getlaporan();
             $this->load->view('laporan',$data);
         }
-        public function search_data_mobil(){
-            $nama = $this->input->post('nama');
+        public function search_data_obat(){
+            $nama_obat = $this->input->post('nama_obat');
             $harga = $this->input->post('harga');
-            $data['data']=$this->m_mobil->search_mobil($nama,$harga);
-            $this->load->view('pilihmobil',$data);
+            $data['data']=$this->m_obat->search_obat($nama_obat,$harga);
+            $this->load->view('tambahobat',$data);
         }
-        public function search_data_motor(){
-            $nama = $this->input->post('nama');
-            $harga = $this->input->post('harga');
-            $data['data']=$this->m_motor->search_motor($nama,$harga);
-            $this->load->view('pilihmotor',$data);
-        }
-        public function proses_tambah_mobil(){
+        public function proses_tambah_obat(){
             $data = array(
-                'nama' => $this->input->post('nama'),
-                'lokasi' => $this->input->post('lokasi'),
-                'harga' => $this->input->post('harga'),
-                'transmisi' => $this->input->post('transmisi'),
-                'tipe' => $this->input->post('tipe'),
+                'nama_obat' => $this->input->post('nama_obat'),
+                'jenis' => $this->input->post('jenis'),
                 'stock' => $this->input->post('stock'),
-                'spek' => $this->input->post('spek'),
+                'harga' => $this->input->post('harga'),
+                'deskripsi' => $this->input->post('deskripsi'),
+                'pict' => $this->input->post('pict'),
             );
-            $this->db->insert('mobil',$data);
-        redirect('C_beranda/tambahmobil');
+            $query
+            $this->db->insert('obat',$data);
+
+        redirect('C_admin/tambahobat');
 	    }
-        public function proses_tambah_motor(){
+        public function proses_update_obat(){
             $data = array(
-                'nama' => $this->input->post('nama'),
-                'lokasi' => $this->input->post('lokasi'),
-                'harga' => $this->input->post('harga'),
-                'transmisi' => $this->input->post('transmisi'),
-                'tipe' => $this->input->post('tipe'),
+                'nama_obat' => $this->input->post('nama'),
+                'jenis' => $this->input->post('jenis'),
                 'stock' => $this->input->post('stock'),
-                'spek' => $this->input->post('spek'),
-            );
-            $this->db->insert('motor',$data);
-        redirect('C_beranda/tambahmotor');
-        }
-        public function proses_update_mobil(){
-            $data = array(
-                'nama' => $this->input->post('nama'),
-                'lokasi' => $this->input->post('lokasi'),
                 'harga' => $this->input->post('harga'),
-                'transmisi' => $this->input->post('transmisi'),
-                'tipe' => $this->input->post('tipe'),
-                'stock' => $this->input->post('stock'),
-                'spek' => $this->input->post('spek'),
+                'deskripsi' => $this->input->post('deskripsi'),
+                'pict' => $this->input->post('pict'),
             );
-            $id_mobil=$this->input->post('id_mobil');
-            $this->db->where('id_mobil',$id_mobil);
-            $this->db->update('mobil',$data);
+            $no_obat=$this->input->post('no_obat');
+            $this->db->where('no_obat',$no_obat);
+            $this->db->update('obat',$data);
 
-            redirect('C_beranda/tambahmobil');
+            redirect('C_admin/tambahobat');
         }        
-        public function proses_delete_mobil(){
-            $this->m_mobil->delete_mobil($this->input->post('id_mobil'));
-            redirect('C_beranda/tambahmobil');
-
-        }
-        public function proses_update_motor(){
-            $data = array(
-                'nama' => $this->input->post('nama'),
-                'lokasi' => $this->input->post('lokasi'),
-                'harga' => $this->input->post('harga'),
-                'transmisi' => $this->input->post('transmisi'),
-                'tipe' => $this->input->post('tipe'),
-                'stock' => $this->input->post('stock'),
-                'spek' => $this->input->post('spek'),
-            );
-            $id_motor=$this->input->post('id_motor');
-            $this->db->where('id_motor',$id_motor);
-            $this->db->update('motor',$data);
-
-            redirect('C_beranda/tambahmotor');
-        }        
-        public function proses_delete_motor(){
-            $this->m_motor->delete_motor($this->input->post('id_motor'));
-            redirect('C_beranda/tambahmotor');
+        public function proses_delete_obat(){
+            $this->m_obat->delete_obat($this->input->post('no_obat'));
+            redirect('C_admin/tambahobat');
 
         }
 	}
