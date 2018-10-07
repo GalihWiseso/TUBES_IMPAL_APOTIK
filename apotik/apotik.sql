0a65fc3f6966d46d2f5a2ea98b5347ddae4b2810
Febry Triyadi <triyadifebry@gmail.com>
2018-10-07 11:33:54 +0700

    Add files via upload

--- apotik/apotik.sql
+++ apotik/apotik.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 18 Sep 2018 pada 17.03
+-- Generation Time: 07 Okt 2018 pada 06.31
 -- Versi Server: 10.1.21-MariaDB
 -- PHP Version: 5.6.30
 
@@ -23,13 +23,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Struktur dari tabel `komplain`
+--
+
+CREATE TABLE `komplain` (
+  `no_komplain` int(20) NOT NULL,
+  `BPJS` int(20) NOT NULL,
+  `kritik` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Struktur dari tabel `laporan`
 --
 
 CREATE TABLE `laporan` (
   `no_laporan` int(20) NOT NULL,
+  `no_obat` int(20) NOT NULL,
   `nama_laporan` varchar(20) NOT NULL,
-  `tanggal` date NOT NULL
+  `tanggal` date NOT NULL,
+  `nama_obat` varchar(20) NOT NULL,
+  `jumlah` int(20) NOT NULL,
+  `total_harga` int(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -44,8 +60,16 @@ CREATE TABLE `obat` (
   `jenis` varchar(20) NOT NULL,
   `stock` int(10) NOT NULL,
   `harga` int(20) NOT NULL,
-  `Deskripsi` varchar(50) NOT NULL
+  `deskripsi` varchar(200) NOT NULL,
+  `pict` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `obat`
+--
+
+INSERT INTO `obat` (`no_obat`, `nama_obat`, `jenis`, `stock`, `harga`, `deskripsi`, `pict`) VALUES
+(0, 'Panadol', 'Sakit kepala', 50, 5000, 'Obat penawar sakit kepala sebelah', 'pict.jpg');
 
 -- --------------------------------------------------------
 
@@ -55,10 +79,43 @@ CREATE TABLE `obat` (
 
 CREATE TABLE `pesanan` (
   `no_pesanan` int(20) NOT NULL,
+  `no_obat` int(20) NOT NULL,
   `BPJS` int(20) NOT NULL,
-  `no_transaksi` int(20) NOT NULL,
   `tanggal_pemesanan` date NOT NULL,
-  `status` varchar(10) NOT NULL
+  `jumlah` int(10) NOT NULL,
+  `total_harga` int(20) NOT NULL,
+  `nama_obat` varchar(20) NOT NULL,
+  `nama` varchar(20) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `alamat_kirim` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `staff`
+--
+
+CREATE TABLE `staff` (
+  `no_staff` int(20) NOT NULL,
+  `nama_staff` varchar(20) NOT NULL,
+  `notlp_staff` varchar(13) NOT NULL,
+  `email_staff` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `supplier`
+--
+
+CREATE TABLE `supplier` (
+  `no_supplier` int(20) NOT NULL,
+  `nama_supplier` varchar(20) NOT NULL,
+  `notlpn_supplier` varchar(13) NOT NULL,
+  `email_supplier` varchar(20) NOT NULL,
+  `password` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -72,7 +129,7 @@ CREATE TABLE `user` (
   `nama` varchar(20) NOT NULL,
   `email` varchar(30) NOT NULL,
   `notlp` varchar(12) NOT NULL,
-  `password` int(30) NOT NULL
+  `password` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -80,10 +137,18 @@ CREATE TABLE `user` (
 --
 
 --
+-- Indexes for table `komplain`
+--
+ALTER TABLE `komplain`
+  ADD PRIMARY KEY (`no_komplain`),
+  ADD KEY `BPJS` (`BPJS`);
+
+--
 -- Indexes for table `laporan`
 --
 ALTER TABLE `laporan`
-  ADD PRIMARY KEY (`no_laporan`);
+  ADD PRIMARY KEY (`no_laporan`),
+  ADD KEY `no_obat` (`no_obat`);
 
 --
 -- Indexes for table `obat`
@@ -92,10 +157,63 @@ ALTER TABLE `obat`
   ADD PRIMARY KEY (`no_obat`);
 
 --
+-- Indexes for table `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD PRIMARY KEY (`no_pesanan`),
+  ADD KEY `pesanan_obat_FK` (`no_obat`),
+  ADD KEY `BPJS` (`BPJS`);
+
+--
+-- Indexes for table `staff`
+--
+ALTER TABLE `staff`
+  ADD PRIMARY KEY (`no_staff`);
+
+--
+-- Indexes for table `supplier`
+--
+ALTER TABLE `supplier`
+  ADD PRIMARY KEY (`no_supplier`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`BPJS`);
+  ADD PRIMARY KEY (`BPJS`),
+  ADD KEY `BPJS` (`BPJS`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `komplain`
+--
+ALTER TABLE `komplain`
+  MODIFY `no_komplain` int(20) NOT NULL AUTO_INCREMENT;
+--
+-- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+--
+
+--
+-- Ketidakleluasaan untuk tabel `komplain`
+--
+ALTER TABLE `komplain`
+  ADD CONSTRAINT `komplain_ibfk_1` FOREIGN KEY (`BPJS`) REFERENCES `user` (`BPJS`);
+
+--
+-- Ketidakleluasaan untuk tabel `laporan`
+--
+ALTER TABLE `laporan`
+  ADD CONSTRAINT `laporan_ibfk_1` FOREIGN KEY (`no_obat`) REFERENCES `obat` (`no_obat`);
+
+--
+-- Ketidakleluasaan untuk tabel `pesanan`
+--
+ALTER TABLE `pesanan`
+  ADD CONSTRAINT `pesanan_ibfk_1` FOREIGN KEY (`BPJS`) REFERENCES `user` (`BPJS`),
+  ADD CONSTRAINT `pesanan_obat_FK` FOREIGN KEY (`no_obat`) REFERENCES `obat` (`no_obat`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
