92bb3955ec3b19a9d92760f221b68c75b7d64455
Febry Triyadi <triyadifebry@gmail.com>
2018-10-07 23:00:12 +0700

    Add files via upload

--- apotik/application/config/routes.php
+++ apotik/application/config/routes.php
@@ -50,5 +50,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |		my-controller/my-method	-> my_controller/my_method
 */
 $route['default_controller'] = 'C_home/index';
+$route['default_controller'] = 'C_login/index';
+$route['default_controller'] = 'C_supplier/index';
+$route['default_controller'] = 'C_admin/index';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
