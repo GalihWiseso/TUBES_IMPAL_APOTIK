139c430dee3d61fba1e7e5716f7b3382ebb268c2
GalihWiseso <galihwiseso@gmail.com>
2018-12-05 20:07:26 +0700

    Nambah fungsi view single obat

--- apotik/application/controllers/C_login.php
+++ apotik/application/controllers/C_login.php
@@ -1,16 +1,16 @@
-<?php 
- 
+<?php
+
 class C_login extends CI_Controller{
 
   function __construct(){
     parent::__construct();
-    $this->load->model('M_user'); 
+    $this->load->model('M_user');
     $this->load->model('m_obat');
     $this->load->library('form_validation');
     $this->load->helper(array('form', 'url'));
     $this->load->library('session');
   }
- 
+
   public function index(){
     if($this->session->userdata('posisi')=='product'){
       #echo $this->session->userdata('posisi');
@@ -31,8 +31,13 @@ class C_login extends CI_Controller{
     $this->load->view('profile');
   }
   public function single(){
-    $this->load->view('single');
-  } 
+    $no_obat =  $this->input->get('nama');
+    $data['data']=$this->m_obat->getNoObat($no_obat);
+  #    $this->load->database();
+  #    $data['data'] = $this->m_obat->getNoObat($no_obat);
+      $this->load->view('single',$data);
+
+  }
   public function proses(){
 
                         $data = [
@@ -47,7 +52,7 @@ class C_login extends CI_Controller{
               $this->session->set_flashdata('message', 'berhasil');
               redirect('C_login/index');
             }else{
-              echo "gagal";   
+              echo "gagal";
                 }
   }
   public function proses_update_akun(){
@@ -65,7 +70,7 @@ class C_login extends CI_Controller{
             $this->db->update('user',$data);
 
             redirect('C_login/profile');
-        }        
+        }
   public function proses_login(){
 
         $uid = $this->input->post('email');
@@ -73,6 +78,9 @@ class C_login extends CI_Controller{
     $hasil=$this->M_user->login($uid,$pwd);
     if($uid=='admin@admin.com' and $pwd=='admin'){
       redirect('C_admin/tambahobat');
+    }
+    else if($uid=='suplier@suplier.com' and $pwd=='suplier') {
+      redirect('C_supplier/index');
     }
     else if ($hasil['exist']>0) {
       $data = $this->M_user->profile($uid,$pwd);
@@ -89,11 +97,11 @@ class C_login extends CI_Controller{
       #$this->index();
     }
     else {
-      $this->session->set_flashdata('message', 'login anda salah, silahkan login kembali');     
+      $this->session->set_flashdata('message', 'login anda salah, silahkan login kembali');
       #echo "gagal";
       redirect('');
       #$this->index();
-    } 
+    }
   }
   public function logout(){
     $this->session->sess_destroy();
