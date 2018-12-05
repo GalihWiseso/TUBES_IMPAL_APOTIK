0f915f15b7c23198155d89e28c2c07b1cbb3dfef
Febry Triyadi <triyadifebry@gmail.com>
2018-12-06 02:14:39 +0700

    Update config

--- apotik/application/config/routes.php
+++ apotik/application/config/routes.php
@@ -49,9 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'C_home/index';
 $route['default_controller'] = 'C_login/index';
-$route['default_controller'] = 'C_supplier/index';
-$route['default_controller'] = 'C_admin/index';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
